16c03fcd1304c25bb951bc8b439994fa742161e3
Ankit sharma <unknownsdeveloper@gmail.com>
2017-06-05 18:19:13 +0530

    Initial commit

---
+++ app/src/main/java/ankit/developer/unknowns/wavesidebaranimation/MainActivity.java
@@ -0,0 +1,29 @@
+package ankit.developer.unknowns.wavesidebaranimation;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void openRightPositionSample(View view) {
+        Intent intent = new Intent(this, RightPositionActivity.class);
+        startActivity(intent);
+    }
+
+    public void openLeftPositionSample(View view) {
+        Intent intent = new Intent(this, LeftPositionActivity.class);
+        startActivity(intent);
+    }
+
+    public void openCustomIndexSample(View view) {
+        Intent intent = new Intent(this, CustomIndexActivity.class);
+        startActivity(intent);
+    }
+}
